bbd152045679ca734d02d2fd80b800d7fed22c9b
sayedelazazy3 <aboanselazazy3@gmail.com>
2026-07-15 02:46:15 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,5 @@
 
-const CACHE='azzazy-v150-FIXED-2COL-V3';
-const ASSETS=['./','./index.html?v=150final2','./manifest.json?v=150final2','./icon-192.png?v=2','./icon-512.png?v=2'];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
+const CACHE='azzazy-final-seal-v6';
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./'])))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));self.clients.claim()});
+self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))});
