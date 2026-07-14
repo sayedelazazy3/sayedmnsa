5e9e37088dfcaccdfb942e5ec6b457fab24a10ab
sayedelazazy3 <aboanselazazy3@gmail.com>
2026-07-15 02:00:43 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+
+const CACHE='azzazy-v150-final';
+const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
