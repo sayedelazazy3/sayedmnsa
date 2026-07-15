1809a4675a08e7a2a38a7d74dab542950cc601ad
sayedelazazy3 <aboanselazazy3@gmail.com>
2026-07-15 05:15:57 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C='azzazy-up-14';self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(['./'])))});self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))))});self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))})
