9f4f28402bb409b4a04e7f35e0e4a97a8795b271
sayedelazazy3 <aboanselazazy3@gmail.com>
2026-07-15 03:48:36 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-
-const CACHE='azzazy-REAL-v10';
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html'])) )});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));self.clients.claim()});
-self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))});
