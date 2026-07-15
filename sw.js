3369dafaad3fd9edb3d6510121c74fd7f9a111cc
sayedelazazy3 <aboanselazazy3@gmail.com>
2026-07-15 17:41:54 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,29 +1,14 @@
-const CACHE='sayed-v12-install-FIXED-01009450493';
+const CACHE='sayed-v13-INSTALL-FIXED-01009450493';
 self.addEventListener('install',e=>{
   self.skipWaiting();
-  console.log('SW v12 - install fixed');
-  e.waitUntil(
-    caches.open(CACHE).then(cache=>{
-      return cache.addAll(['./','./index.html?v=12','./manifest.json?v=12','./icon-192.png','./icon-512.png']).catch(err=>console.log('cache add fail',err));
-    })
-  );
+  console.log('SW v13 install - INSTALL FIXED');
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html?v=13','./manifest.json?v=13','./icon-192.png','./icon-512.png']).catch(()=>{})));
 });
 self.addEventListener('activate',e=>{
-  e.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.map(k=>{
-      if(k!==CACHE){
-        console.log('deleting old cache',k);
-        return caches.delete(k);
-      }
-    }))).then(()=>self.clients.claim())
-  );
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>{
+    if(k!==CACHE){console.log('deleting old cache that breaks install',k);return caches.delete(k);}
+  }))).then(()=>self.clients.claim()));
 });
 self.addEventListener('fetch',e=>{
-  e.respondWith(
-    fetch(e.request).then(r=>{
-      const clone=r.clone();
-      caches.open(CACHE).then(c=>c.put(e.request,clone));
-      return r;
-    }).catch(()=>caches.match(e.request))
-  );
+  e.respondWith(fetch(e.request).then(r=>{caches.open(CACHE).then(c=>c.put(e.request,r.clone()));return r;}).catch(()=>caches.match(e.request)));
 });
