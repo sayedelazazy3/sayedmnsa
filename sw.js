4adb33c283550aa180af63890311eb8138ef3362
sayedelazazy3 <aboanselazazy3@gmail.com>
2026-07-16 15:14:58 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='sayed-v16-SECURE-01009450493';
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html?v=16','./manifest.json?v=16','./icon-192.png','./icon-512.png']).catch(()=>{})));});
+const CACHE='sayed-v175-FINAL-BEBASATA-01009450493';
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html?v=175','./manifest.json?v=175','./icon-192.png','./icon-512.png']).catch(()=>{})));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>{if(k!==CACHE)return caches.delete(k);}))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).then(r=>{caches.open(CACHE).then(c=>c.put(e.request,r.clone()));return r;}).catch(()=>caches.match(e.request)));});
